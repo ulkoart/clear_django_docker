68749d97fd67a92004913581e3502c02f855429f
Artem Ulko <ulkoart@gmail.com>
2018-10-18 11:55:27 +0300

    wait for db

--- srс/docker-entrypoint.sh
+++ srс/docker-entrypoint.sh
@@ -1,5 +1,27 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
+set -e
+
+host="$1"
+shift
+
+until psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+# Start Collectstatic
+echo Collectstatic.
 python manage.py collectstatic -v0 --noinput -c
+
+# Start migrate
+echo Migrate.
 python manage.py migrate -v0 --noinput
-gunicorn project.wsgi -b :8000 -w 2 --timeout 1200
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn project.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 2
